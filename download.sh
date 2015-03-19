c6704fd69217601688a817aa15916c4ca4378303
Jakob Stoeck <j@jakobstoeck.de>
2015-03-19 21:19:12 +0100

    update

--- download.sh
+++ download.sh
@@ -3,7 +3,8 @@ watch -n5 '\
 for i in 2322696 2322700; do \
 curl -s "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | jq '.' > "liveticker_ma${i}.json" \
 && curl -s "http://sportsapi.sport1.de/match/ma${i}" | jq '.' > "match_ma${i}.json" \
-&& curl -s "http://sportsapi.sport1.de/match-event/ma${i}" | jq '.' > "match-event_ma${i}.json"; \
+&& curl -s "http://sportsapi.sport1.de/match-event/ma${i}" | jq '.' > "match-event_ma${i}.json" \
+&& curl -s "http://sport1-xml-old.c.ergebnis-dienst.de/match/ma2309741" > "match_old_ma${i}.xml"; \
 done; \
 curl -sO http://lb.iph.sp1-tek.de/v8/media/live.xml; \
 curl -sO http://mobile.ipnm.sport1.de/v8/media/live.xml; \
