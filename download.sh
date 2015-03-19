bdcde749c80f80d4cb5c757911bfac9a789cd34e
Jakob Stoeck <j@jakobstoeck.de>
2015-03-19 22:56:22 +0100

    update

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 watch -n5 '\
-for i in 2322696 2322700; do \
+for i in 2322696 2322700 2309743 2309737; do \
 curl -s "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | jq '.' > "liveticker_ma${i}.json" \
 && curl -s "http://sportsapi.sport1.de/match/ma${i}" | jq '.' > "match_ma${i}.json" \
 && curl -s "http://sportsapi.sport1.de/match-event/ma${i}" | jq '.' > "match-event_ma${i}.json" \
@@ -8,7 +8,7 @@ curl -s "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | jq '.' > "livetick
 done; \
 curl -sO http://lb.iph.sp1-tek.de/v8/media/live.xml; \
 curl -sO http://mobile.ipnm.sport1.de/v8/media/live.xml; \
-for j in 439815; do \
+for j in 439815 439817 439811; do \
 curl -sO "http://mobile.ipnm.sport1.de/v8/soccer/leagues/5/game/${j}.xml" \
 && curl -s http://mobile.ipnm.sport1.de/v8/soccer/leagues/5/conference.xml > conference5.xml; \
 done; \
