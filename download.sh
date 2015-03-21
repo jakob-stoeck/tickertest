547cc1567273a6b3ddf92974435138b1a74be072
Jakob Stoeck <j@jakobstoeck.de>
2015-03-21 16:32:54 +0100

    update

--- download.sh
+++ download.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 watch -n5 '\
-for i in 2322696 2322700 2309743 2309737; do \
+for i in 2226958 2226986 2226988 2226996 2227000 2226966; do \
 curl -s "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | jq '.' > "liveticker_ma${i}.json" \
 && curl -s "http://sportsapi.sport1.de/match/ma${i}" | jq '.' > "match_ma${i}.json" \
 && curl -s "http://sportsapi.sport1.de/match-event/ma${i}" | jq '.' > "match-event_ma${i}.json" \
-&& curl -s "http://sport1-xml-old.c.ergebnis-dienst.de/match/ma2309741" > "match_old_ma${i}.xml"; \
+&& curl -s "http://sport1-xml-old.c.ergebnis-dienst.de/match/ma${i}/" > "match_old_ma${i}.xml"; \
 done; \
 curl -sO http://lb.iph.sp1-tek.de/v8/media/live.xml; \
 curl -sO http://mobile.ipnm.sport1.de/v8/media/live.xml; \
-for j in 439815 439817 439811; do \
-curl -sO "http://mobile.ipnm.sport1.de/v8/soccer/leagues/5/game/${j}.xml" \
-&& curl -s http://mobile.ipnm.sport1.de/v8/soccer/leagues/5/conference.xml > conference5.xml; \
+for j in 407556 407584 407586 407594 407598 407564; do \
+curl -sO "http://mobile.ipnm.sport1.de/v8/soccer/leagues/1/game/${j}.xml" \
+&& curl -s http://mobile.ipnm.sport1.de/v8/soccer/leagues/1/conference.xml > conference1.xml; \
 done; \
 git add . && git commit -m "update"'
 
