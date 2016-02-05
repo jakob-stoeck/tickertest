0051f48cc1ab0ab3a82471e96ac198c03b7057f8
Jakob Stoeck <j@jakobstoeck.de>
2016-02-05 19:22:23 +0100

    update

--- download.sh
+++ download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 watch -n5 '\
 for i in 2415975 2416108 2416115 2416129; do \
-curl --compressed -s "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | jq '.' > "liveticker_ma${i}.json" \
-&& curl --compressed -s "http://sportsapi.sport1.de/match/ma${i}" | jq '.' > "match_ma${i}.json" \
-&& curl --compressed -s "http://sportsapi.sport1.de/match-event/ma${i}" | jq '.' > "match-event_ma${i}.json"; \
+curl --compressed -is "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | json > "liveticker_ma${i}.json" \
+&& curl --compressed -is "http://sportsapi.sport1.de/match/ma${i}" | json > "match_ma${i}.json" \
+&& curl --compressed -is "http://sportsapi.sport1.de/match-event/ma${i}" | json > "match-event_ma${i}.json"; \
 done; \
-curl --compressed -s "http://sportsapi.sport1.de/matches-by-date/da2016-02-05" | jq '.' > "matches-by-date2016-02-05.json"; \
-curl --compressed -s http://mobile.ipnm.sport1.de/v8/media/live.xml | xmllint --format - > live.xml; \
+curl --compressed -is "http://sportsapi.sport1.de/matches-by-date/da2016-02-05" | json > "matches-by-date2016-02-05.json"; \
+curl --compressed -is http://mobile.ipnm.sport1.de/v8/media/live.xml | xmllint --format - > live.xml; \
 git add . && git commit -m "update"'
 
 
