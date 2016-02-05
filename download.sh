f1f8c2e6c47f04733c282b6013d0f2da3cc01f98
Jakob Stoeck <j@jakobstoeck.de>
2016-02-05 19:25:56 +0100

    update

--- download.sh
+++ download.sh
@@ -6,7 +6,7 @@ curl --compressed -is "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | json
 && curl --compressed -is "http://sportsapi.sport1.de/match-event/ma${i}" | json > "match-event_ma${i}.json"; \
 done; \
 curl --compressed -is "http://sportsapi.sport1.de/matches-by-date/da2016-02-05" | json > "matches-by-date2016-02-05.json"; \
-curl --compressed -is http://mobile.ipnm.sport1.de/v8/media/live.xml | xmllint --format - > live.xml; \
+curl --compressed -s http://mobile.ipnm.sport1.de/v8/media/live.xml | xmllint --format - > live.xml; \
 git add . && git commit -m "update"'
 
 
