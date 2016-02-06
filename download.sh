ef7c1efc69468111c865b4da57544d30fd94882b
Jakob Stoeck <j@jakobstoeck.de>
2016-02-06 15:05:09 +0100

    update

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 watch -n5 '\
-for i in 2415975 2416108 2416115 2416129; do \
+for i in 2415952 2415961 2415968 2415972 2415987 2415944; do \
 curl --compressed -s "http://sportsapi.sport1.de/liveticker/co19/ma${i}" | jq '.' > "liveticker_ma${i}.json" \
 && curl --compressed -s "http://sportsapi.sport1.de/match/ma${i}" | jq '.' > "match_ma${i}.json" \
 && curl --compressed -s "http://sportsapi.sport1.de/match-event/ma${i}" | jq '.' > "match-event_ma${i}.json"; \
 done; \
-curl --compressed -s "http://sportsapi.sport1.de/matches-by-date/da2016-02-05" | jq '.' > "matches-by-date2016-02-05.json"; \
+curl --compressed -s "http://sportsapi.sport1.de/matches-by-date/da2016-02-06" | jq '.' > "matches-by-date2016-02-05.json"; \
 curl --compressed -s http://mobile.ipnm.sport1.de/v8/media/live.xml | xmllint --format - > live.xml; \
 git add . && git commit -m "update"'
 
